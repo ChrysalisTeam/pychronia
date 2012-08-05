31ff86e57955718db08beaa01c43ff219fa9c268
PKL <pythoniks@gmail.com>
2012-08-05 18:41:53 +0200

    Added string_similarity() utility.

--- rpgweb/utilities/__init__.py
+++ rpgweb/utilities/__init__.py
@@ -165,6 +165,9 @@ def check_object_tree(tree, allowed_types, path):
 
 
 def substract_lists(available_gems, given_gems):
+    """
+    Returns None if operation is impossible.
+    """
     available_gems = Counter(available_gems)
     given_gems = Counter(given_gems)
 
@@ -173,6 +176,31 @@ def substract_lists(available_gems, given_gems):
 
     gems_remaining = available_gems - given_gems
     return PersistentList(gems_remaining.elements())
+
+
+def string_similarity(first, second):
+    """Find the Levenshtein distance between two strings.
+    Returns a positive integer, with 0 <-> identity."""
+    if len(first) > len(second):
+        first, second = second, first
+    if len(second) == 0:
+        return len(first)
+    first_length = len(first) + 1
+    second_length = len(second) + 1
+    distance_matrix = [[0] * second_length for _ in range(first_length)]
+    for i in range(first_length):
+        distance_matrix[i][0] = i
+    for j in range(second_length):
+        distance_matrix[0][j]=j
+    for i in xrange(1, first_length):
+        for j in range(1, second_length):
+            deletion = distance_matrix[i-1][j] + 1
+            insertion = distance_matrix[i][j-1] + 1
+            substitution = distance_matrix[i-1][j-1]
+            if first[i-1] != second[j-1]:
+                substitution += 1
+            distance_matrix[i][j] = min(insertion, deletion, substitution)
+    return distance_matrix[first_length-1][second_length-1]
 
 
 def sanitize_query_dict(query_dict):
