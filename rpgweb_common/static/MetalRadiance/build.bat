e9c5ae96abab58c8c539e90c6392098b0f194e59
PKL <pythoniks@gmail.com>
2013-02-07 22:22:24 +0100

    Transformed mobile design to SCSS.

--- rpgweb_common/static/MetalRadiance/build.bat
+++ rpgweb_common/static/MetalRadiance/build.bat
@@ -1,8 +1,9 @@
 :loop
 
 python -m scss scss/main.scss -o metalradiance.css -I scss -C 
-rem --debug-info -C
-  
+
+python -m scss scss/main_mobile.scss -o metalradiance_mobile.css -I scss -C 
+
 rem ;;;;python -m scss -w scss --recursive -o css -I scss
 rem ; --debug-info 
 
