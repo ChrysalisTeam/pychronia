2cb199fbd00ad29725724a571999541148eae9dc
PKL <pythoniks@gmail.com>
2013-06-19 22:02:18 +0200

    Failed re-attempt at using pyScss watchdog (doesn't care about includes and needs --recursive..).

--- pychronia_common/static/MetalRadiance/build.bat
+++ pychronia_common/static/MetalRadiance/build.bat
@@ -4,8 +4,7 @@ python -m scss scss/main.scss -o metalradiance.css -I scss -C
 
 python -m scss scss/main_mobile.scss -o metalradiance_mobile.css -I scss -C 
 
-rem ;;;;python -m scss -w scss --recursive -o css -I scss
-rem ; --debug-info 
+rem python -m scss -r -C -I scss -w .\scss -o . --debug-info
 
 TIMEOUT /T 5
 goto loop
