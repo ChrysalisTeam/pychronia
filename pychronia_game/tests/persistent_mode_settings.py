3e7e6b1861d339fc9c82f566200b530c4f0c4ccf
PKL <pythoniks@gmail.com>
2013-11-11 22:09:36 +0100

    Made common setting fixtures for dev and integration environments.

--- pychronia_game/tests/persistent_mode_settings.py
+++ pychronia_game/tests/persistent_mode_settings.py
@@ -26,21 +26,14 @@ ZODB_URL = None
 
 
 
-## FOR INTEGRATION ##
-ACTIVATE_AIML_BOTS = True
-##GAME_FILES_ROOT = "P:\\Chrysalis Depot\\Chrysalis V1\\"
-##GAME_INITIAL_DATA_PATH = r"P:\Chrysalis Depot\Chrysalis V1\script_fixtures"
-
 
 PASSWORDS_POOL = [str(random.randint(100, 1000)) for i in range(50)]
-
 
 
 def GAME_INITIAL_FIXTURE_SCRIPT(dm):
     """
     Called just before conversion of initial data tree, and coherency check.
     """
-
     logging.info("Loading special game fixture script...")
 
     #return # TODO TEMPORARY
@@ -50,16 +43,20 @@ def GAME_INITIAL_FIXTURE_SCRIPT(dm):
     activable_views = dm.ACTIVABLE_VIEWS_REGISTRY.keys()
     dm.set_activated_game_views(activable_views)
 
-    # we give guy1 access to everything
-    dm.update_permissions("guy1", PersistentList(dm.PERMISSIONS_REGISTRY))
+
+    player_name, player_name_bis = dm.get_character_usernames()[0:2]
+
+    # we give first player access to everything
+    assert not dm.get_character_properties(player_name)["is_npc"]
+    dm.update_permissions(player_name, PersistentList(dm.PERMISSIONS_REGISTRY))
 
     # we can see all articles
     dm.set_encyclopedia_index_visibility(True)
 
     # we fill the messages
-    email_guy1 = dm.get_character_email("guy1")
-    email_guy2 = dm.get_character_email("guy2")
-    email_master = "judicators@acharis.com"
+    email_guy1 = dm.get_character_email(player_name)
+    email_guy2 = dm.get_character_email(player_name_bis)
+    email_external = sorted(dm.global_contacts.keys())[0]
 
     msg_id1 = dm.post_message(sender_email=email_guy1, recipient_emails=email_guy2, subject="message1", body="hello")
     msg1 = dm.get_dispatched_message_by_id(msg_id1)
@@ -67,17 +64,28 @@ def GAME_INITIAL_FIXTURE_SCRIPT(dm):
     msg2 = dm.get_dispatched_message_by_id(msg_id2)
     msg_id3 = dm.post_message(sender_email=email_guy1, recipient_emails=email_guy2, subject="Bis:%s" % msg2["subject"], body="hello hello", parent_id=msg_id2)
 
-    msg_id4 = dm.post_message(sender_email=email_guy1, recipient_emails=email_master, subject="Ask master", body="for something")
+    msg_id4 = dm.post_message(sender_email=email_guy1, recipient_emails=email_external, subject="Ask master", body="for something")
     msg4 = dm.get_dispatched_message_by_id(msg_id4)
-    msg_id5 = dm.post_message(sender_email=email_master, recipient_emails=email_guy1, subject="RE:%s" % msg4["subject"], body="answer something", parent_id=msg_id4)
+    msg_id5 = dm.post_message(sender_email=email_external, recipient_emails=email_guy1, subject="RE:%s" % msg4["subject"], body="answer something", parent_id=msg_id4)
     msg5 = dm.get_dispatched_message_by_id(msg_id5)
-    msg_id6 = dm.post_message(sender_email=email_guy1, recipient_emails=email_master, subject="Bis:%s" % msg5["subject"], body="ask for something", parent_id=msg_id5)
+    msg_id6 = dm.post_message(sender_email=email_guy1, recipient_emails=email_external, subject="Bis:%s" % msg5["subject"], body="ask for something", parent_id=msg_id5)
 
     # we distribute auction items
-    dm.transfer_object_to_character("statue", "guy1")
-    dm.transfer_object_to_character("several_misc_gems", "guy2")
+    gem = dm.get_gem_items().keys()[0]
+    dm.transfer_object_to_character(gem, player_name)
+    item = dm.get_non_gem_items().keys()[0]
+    dm.transfer_object_to_character(item, player_name)
 
-    dm.propose_friendship("guy1", "guy2")
-    dm.propose_friendship("guy2", "guy1") # accept friendship
+    dm.propose_friendship(player_name, player_name_bis)
+    dm.propose_friendship(player_name_bis, player_name) # accept friendship
 
+
+
+
+
+## OVERRIDES FOR INTEGRATION OF CHRYSALIS GAME ##
+
+ACTIVATE_AIML_BOTS = False
+GAME_FILES_ROOT = r"P:\Chrysalis Depot\Chrysalis V1\ ".strip()
+GAME_INITIAL_DATA_PATH = r"P:\Chrysalis Depot\Chrysalis V1\script_fixtures"
 
