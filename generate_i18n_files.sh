6bf4ae08b0eefde58a7dc0e7599909021434e490
PKL <pythoniks@gmail.com>
2015-03-07 20:55:48 +0100

    Fix generate_i18n_files script.

--- generate_i18n_files.sh
+++ generate_i18n_files.sh
@@ -2,9 +2,9 @@
 for mydir in pychronia_game pychronia_cms pychronia_common 
 do
     pushd $mydir
-    python ../manage.py makemessages -l fr
+    python tests/manage.py makemessages -l fr
     # then use virtaal to translate messages efficiently
-    python ../manage.py compilemessages
+    python tests/manage.py compilemessages
     popd
 done
  
