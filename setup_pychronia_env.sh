930728c4b9ceeca3ba49831c53c748f66241bd57
PKL <pythoniks@gmail.com>
2015-03-14 21:35:49 +0100

    Add virtualenv activation to setup_pychronia_env.sh

--- setup_pychronia_env.sh
+++ setup_pychronia_env.sh
@@ -2,6 +2,8 @@
 
 SCRIPTDIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
+source $SCRIPTDIR/../ENV/bin/activate
+
 export PYTHONPATH="$SCRIPTDIR:$SCRIPTDIR/dependencies"
 
 if [ -z "$DJANGO_SETTINGS_MODULE" ]
