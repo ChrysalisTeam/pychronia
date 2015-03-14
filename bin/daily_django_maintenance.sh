f1a3461ec682c7549d2b80f9bd003d8c204a315b
PKL <pythoniks@gmail.com>
2015-03-14 22:44:27 +0100

    Add proper error code handling to daily_django_maintenance.sh

--- bin/daily_django_maintenance.sh
+++ bin/daily_django_maintenance.sh
@@ -1,5 +1,13 @@
 
+res=0
 
 python manage.py clearsessions
+res=$(($res + $?))
+
 python manage.py thumbnail_cleanup
+res=$(($res + $?))
+
 python manage.py purgerequests --noinput 1 month
+res=$(($res + $?))
+
+exit $res
