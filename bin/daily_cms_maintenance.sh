24c30a1b43ea19b0e2faca0d7226c423becc3dde
PKL <pythoniks@gmail.com>
2015-03-15 19:31:09 +0100

    Add new daily_game_maintenance.sh, and titles in daily_cms_maintenance.sh.

--- bin/daily_cms_maintenance.sh
+++ bin/daily_cms_maintenance.sh
@@ -1,12 +1,15 @@
 
 res=0
 
+echo -e "\n***CLEARING OLD DJANGO SESSIONS***\n"
 python manage.py clearsessions
 res=$(($res + $?))
 
+echo -e "\n***CLEARING OLD DJANGO THUMBNAILS***\n"
 python manage.py thumbnail_cleanup
 res=$(($res + $?))
 
+echo -e "\n***CLEARING OLD DJANGO REQUEST STATS***\n"
 python manage.py purgerequests --noinput 1 month
 res=$(($res + $?))
 
---
+++ bin/daily_game_maintenance.sh
@@ -0,0 +1,33 @@
+
+res=0
+
+echo -e "\n***CLEARING OLD DJANGO SESSIONS***\n"
+python manage.py clearsessions
+res=$(($res + $?))
+
+
+echo -e "\n***MAKING BACKUP OF PYCHRONIA GAMES***\n"
+python -c "from pychronia_game.scripts import backup_all_games; backup_all_games.execute()"
+res=$(($res + $?))
+
+echo -e "\n***MAKING SANITY CHECK OF PYCHRONIA GAMES***\n"
+python -c "from pychronia_game.scripts import check_global_sanity; check_global_sanity.execute()"
+res=$(($res + $?))
+
+echo -e "\n***RESETTING DEMO OF PYCHRONIA GAME***\n"
+python -c "from pychronia_game.scripts import reset_demo_account; reset_demo_account.execute()"
+res=$(($res + $?))
+
+echo -e "\n***PACKING ZODB DATABASE***\n"
+python dependencies/relstorage/zodbpack.py -d 10 zodbpack.conf
+res=$(($res + $?))
+
+
+# NOT FOR NOW, TO BE ACTIVATED SOON #
+#echo -e "\n***NOTIFYING NOVELTIES PYCHRONIA GAMES***\n"
+#python -c "from pychronia_game.scripts import notify_novelties_by_email; notify_novelties_by_email.execute()"
+#res=$(($res + $?))
+
+exit $res
+
+
