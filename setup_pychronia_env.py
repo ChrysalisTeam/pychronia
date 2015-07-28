1d1111e4f5a453bdb5fa9b5533749218a59f2666
PKL <pythoniks@gmail.com>
2015-07-28 19:18:50 +0200

    Fix the order of steps in setup_pychronia_env.py

--- setup_pychronia_env.py
+++ setup_pychronia_env.py
@@ -1,11 +1,6 @@
 
 import os, sys
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pychronia_settings")  # not erased if already set, eg. for tests
-
-import django
-django.setup()
-
 root = os.path.dirname(os.path.realpath(__file__))
 if root not in sys.path:
     sys.path.insert(0, root)
@@ -13,3 +8,9 @@ if root not in sys.path:
 dependencies = os.path.join(root, "dependencies")
 if dependencies not in sys.path:
     sys.path.insert(0, dependencies)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pychronia_settings")  # not erased if already set, eg. for tests
+
+import django
+django.setup()
+
