efd631ab40700168ba04f71ded261edf1c35fb62
responsivedesign <chaki.abderrazak@gmail.com>
2015-08-03 16:15:28 +0200

    Add a media queris and a new css link for the new design

--- pychronia_common/static/MetalRadiance/build.bat
+++ pychronia_common/static/MetalRadiance/build.bat
@@ -4,6 +4,8 @@ python -m scss scss/main.scss -o metalradiance.css -I scss -C
 
 python -m scss scss/main_mobile.scss -o metalradiance_mobile.css -I scss -C
 
+python -m scss scss/main_responsive.scss -o metalradiance_responsive.css -I scss -C
+
 rem python -m scss -r -C -I scss -w .\scss -o . --debug-info
 
 TIMEOUT /T 5
