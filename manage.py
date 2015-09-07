6db58baadbe13f0c16b33b4ab8b0be1fbf557cf9
PKL <pythoniks@gmail.com>
2015-09-07 21:30:52 +0200

    Slight doc update for new contributors.

--- manage.py
+++ manage.py
@@ -4,6 +4,12 @@ import sys
 
 import setup_pychronia_env
 
+"""
+Django manage.py script intended for use in PREVIEW/PROD environments, with a custom pychronia_settings.py file.
+
+See /tests subfolders to manage DEV environments.
+"""
+
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
