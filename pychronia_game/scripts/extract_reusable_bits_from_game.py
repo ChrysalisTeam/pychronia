4ae702adc8c999805e90298fd5cdb4698556ade8
PKL <pythoniks@gmail.com>
2015-09-08 19:55:19 +0200

    Implement draft of extract_reusable_bits_from_game.py script.

---
+++ pychronia_game/scripts/extract_reusable_bits_from_game.py
@@ -0,0 +1,36 @@
+ #!/usr/bin/env python
+ # -*- coding: utf-8 -*-
+import os
+import sys
+import pprint
+import logging
+import setup_pychronia_env
+
+import pychronia_game.models # initializes everything
+from pychronia_game.datamanager.datamanager_administrator import UsageError, retrieve_game_instance
+from pychronia_game.utilities import dump_data_tree_to_yaml
+
+
+def execute():
+    instance_id = sys.argv[1]  # please provide game instance id on cmd line
+    dm = retrieve_game_instance(instance_id)
+
+    msgs = dm.get_all_dispatched_messages()
+
+    tpls = [dm.convert_msg_to_template(msg) for msg in msgs]
+    #pprint.pprint(tpls)
+
+    data = dump_data_tree_to_yaml(tpls, width=50, default_style=None)
+
+    filename = "%s_extracted_message_templates.yaml" % instance_id
+    with open(filename, "wb") as f:
+        f.write(data)
+
+    print(data)
+    print(">> Extract file %s successfully created" % filename)
+
+
+
+if __name__ == "__main__":
+    execute()
+
