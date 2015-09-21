ec95471a6820cf55fdef2f9a54d2b38b92ba92c3
PKL <pythoniks@gmail.com>
2015-09-21 22:52:19 +0200

    Enhance SCSS builder with "watchmedo" script, from python-watchdog package.

--- pychronia_common/static/MetalRadiance/build.bat
+++ pychronia_common/static/MetalRadiance/build.bat
@@ -1,11 +1,13 @@
 :loop
 
+watchmedo shell-command scss -c "python -m scss scss/metalradiance.scss -o metalradiance.css -I scss -I ../bootstrap-sass/assets/stylesheets -I ../bootstrap-sass/assets/stylesheets/bootstrap -C"
+
 rem python -m scss scss/metalradiance.scss -o metalradiance.css -I scss -I ../bootstrap-sass/assets/stylesheets -I ../bootstrap-sass/assets/stylesheets/bootstrap -C
 rem TIMEOUT /T 5
 rem goto loop
 
 rem BEWARE - watchdog only recompiles *.scss files WITHOUT underscore prefix, so "touch" metalradiance.scss to trigger it 
-python -m scss -w scss -o . -I scss -I ../bootstrap-sass/assets/stylesheets -I ../bootstrap-sass/assets/stylesheets/bootstrap -C
+rem python -m scss -w scss -o . -I scss -I ../bootstrap-sass/assets/stylesheets -I ../bootstrap-sass/assets/stylesheets/bootstrap -C
 
 rem : --debug-info
 
