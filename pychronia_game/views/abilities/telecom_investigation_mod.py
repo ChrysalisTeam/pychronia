51300cb81c31aa841ce73b53858d26a8073e8488
« NouryG » <michaelfr33man@gmail.com>
2016-02-03 14:52:08 +0100

    conversation w/ template

--- pychronia_game/views/abilities/telecom_investigation_mod.py
+++ pychronia_game/views/abilities/telecom_investigation_mod.py
@@ -8,6 +8,8 @@ from pychronia_game.datamanager.abstract_game_view import register_view
 from pychronia_game.datamanager.datamanager_tools import readonly_method, \
     transaction_watcher
 from pychronia_game.forms import OtherCharactersForm
+from django.utils.html import strip_tags
+from django.template.loader import render_to_string
 
 @register_view
 class TelecomInvestigationAbility(AbstractAbility):
@@ -53,28 +55,56 @@ class TelecomInvestigationAbility(AbstractAbility):
     
         visibility_reasons = None
         archived=None
-        conversations = "\n"
     
         result = self.get_user_related_messages(target_username, visibility_reasons, archived)
     
         conv = self.sort_messages_by_conversations(result)
-    
-        for i in range (0, len(conv)):
-            conversations += ("**" + _("Conversation number %(numero)s :") % dict(numero=(i+1)) + "**" + "\n\n")
-            recipient_num = len(conv[i][0]["recipient_emails"])
-            for j in range(0, recipient_num):
-                conversations += ( _("Parcticipants :  ") + conv[i][0]["sender_email"] + _(" ; ") + str(conv[i][0]["recipient_emails"][j]) + "\n\n")
-            num = len(conv[i])
-            conversations += ( _("Subject of conversation :  ") + conv[i][num-1]["subject"] + "\n\n")
-            conversations += ( _("Number of messages exchanged :  ") + str(num) + "\n\n")
-            conversations += ( _("First sent message :  ") + str(conv[i][num-1]["sent_at"]) + "\n\n")
-            conversations += ( _("Last sent message :  ") +  str(conv[i][0]["sent_at"]) + "\n\n")
+        context_list=[]
         
-        return conversations
+        if conv == []:
+            context_list = []
+            
+        else:
+            """for i in range (0, len(conv)):
+                num = len(conv[i])
+                recipient_num = len(conv[i][0]["recipient_emails"])
+                #conversations += ("**" + _("Conversation number %(numero)s :") % dict(numero=(i+1)) + "**" + "\n\n")
+                conversations += ( _(" %(sujet)s :") % dict(sujet=conv[i][num-1]["subject"]) + "\n\n")
+                for j in range(0, recipient_num):
+                    conversations += ( _("Parcticipants :  ") + conv[i][0]["sender_email"] + _(" ; ") + str(conv[i][0]["recipient_emails"][j]) + "\n\n")
+                #conversations += ( _("Subject of conversation :  ") + conv[i][num-1]["subject"] + "\n\n")
+                conversations += ( _("Number of messages exchanged :  ") + str(num) + "\n\n")
+                conversations += ( _("First sent message :  ") + str(conv[i][num-1]["sent_at"]) + "\n\n")
+                conversations += ( _("Last sent message :  ") +  str(conv[i][0]["sent_at"]) + "\n\n")
+                conversations += ("\n\n")"""
+                    
+            for i in range (0, len(conv)):
+                num = len(conv[i])
+                recipient_num = len(conv[i][0]["recipient_emails"])
+                subject = conv[i][num-1]["subject"]
+                first_message = conv[i][num-1]["sent_at"]
+                last_message = conv[i][0]["sent_at"]
+                sender = conv[i][0]["sender_email"]
+                for j in range (0, recipient_num):
+                    recipients = conv[i][0]["recipient_emails"][j]
+                context = {"subject" : subject, "messages" : num, "sender" : sender, "recipients" : recipients, "first_message" : first_message, "last_message" : last_message}
+                context_list = context_list + [context]
+        
+            return context_list
+
+    def conversation_formatting(self,context_list):
+        body=""
+        if context_list == None:
+            body = _("La cible n'a aucune conversation!")
+        else:
+            for i in range (0, len(context_list)):
+                body += render_to_string("abilities/telecom_summary_format.html", context_list[i])
+        return body
 
     @transaction_watcher
     def process_telecom_investigation(self, target_username, use_gems=()):
         
+        body2=""
         target_name = self.get_official_name(target_username)
         user_email = self.get_character_email()
         remote_email = "investigator@spies.com"
@@ -89,7 +119,9 @@ class TelecomInvestigationAbility(AbstractAbility):
         #answer e-mail:
         subject = _('<Investigation Results for %(target_name)s>') % dict(target_name=target_name)
         
-        body = self.extract_conversation_summary(target_username)
+        context_list = self.extract_conversation_summary(target_username)
+        
+        body = self.conversation_formatting(context_list)
         
         msg_id = self.post_message(remote_email, user_email, subject, body, date_or_delay_mn=0)
 
