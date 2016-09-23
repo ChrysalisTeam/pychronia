71d21af9810d154795e1a1a6f387f797d206467c
PKL <pythoniks@gmail.com>
2016-09-23 10:06:37 +0200

    Remove vendored jplayer files.

--- dependencies/jplayer/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.admin.sites import site
-from models import JPlayer, Song, Artist, Credits
-
-site.register(JPlayer)
-site.register(Song)
-site.register(Artist)
-site.register(Credits)
