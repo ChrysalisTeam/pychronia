a733dbbafc70d9764fe5c1643796abcb1d9f6ad1
PKL <pythoniks@gmail.com>
2016-10-04 15:13:46 +0200

    Silence python warnings by default, using pychronia_logging_config.

--- pychronia_common/default_logging_config.py
+++ pychronia_common/default_logging_config.py
@@ -41,6 +41,13 @@ pychronia_logging_config = {
             'level': 'INFO',
             'propagate': True
         },
+
+        'py.warnings': {
+            'handlers': [],
+            'level': 'ERROR',  # this SILENCES warnings!
+            'propagate': True
+        },
+
         'pychronia_game': {
             'handlers': ['game_instance'],
             'level': 'INFO',
@@ -74,51 +81,3 @@ pychronia_logging_config = {
 
 logging.config.dictConfig(pychronia_logging_config)
 logging.disable(logging.DEBUG)  # global limit
-
-'''
-DEFAULT_LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse',
-        },
-        'require_debug_true': {
-            '()': 'django.utils.log.RequireDebugTrue',
-        },
-    },
-    'handlers': {
-        'console': {
-            'level': 'INFO',
-            'filters': ['require_debug_true'],
-            'class': 'logging.StreamHandler',
-        },
-        'null': {
-            'class': 'logging.NullHandler',
-        },
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-        },
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': False,
-        },
-        'django.security': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': False,
-        },
-        'py.warnings': {
-            'handlers': ['console'],
-        },
-    }
-}
-'''
