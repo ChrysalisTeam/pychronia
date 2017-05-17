b206e1eb0d07dc20660de12f8d9136ab46a5765f
PKL <pythoniks@gmail.com>
2017-05-17 16:35:40 +0200

    Fix buggy empty pattern in i18n_patterns().

--- pychronia_cms/urls.py
+++ pychronia_cms/urls.py
@@ -43,8 +43,7 @@ urlpatterns = patterns('',
                        url(r'^i18n/', include('django.conf.urls.i18n')),  # to set language
                        )
 
-urlpatterns += i18n_patterns('',
-
+urlpatterns += i18n_patterns(
                              #(r'^accounts/', include('userprofiles.urls')), # one-step registration
 
                              url(r'^weblog/', include('zinnia.urls')),
