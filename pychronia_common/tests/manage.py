4ee7619346e98a9835b361dc109ce2ba42ce2a93
PKL <pythoniks@gmail.com>
2017-05-19 17:27:00 +0200

    Tweak and fix styling of menus and standard contents.

--- pychronia_common/tests/manage.py
+++ pychronia_common/tests/manage.py
@@ -4,6 +4,7 @@ import os, sys
 
 os.environ[
     "DJANGO_SETTINGS_MODULE"] = settings_module = "pychronia_common.tests.persistent_mode_settings"  # with DB not in temp dir
+
 import setup_pychronia_env  # only AFTER setting this DJANGO_SETTINGS_MODULE
 
 from django.core.management import execute_from_command_line
