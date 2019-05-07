b90a9c85c37c5bef77ac1ff8996394ddb13d8dc3
PKL <pythoniks@gmail.com>
2019-05-07 11:55:53 +0200

    Update manage.py of pychronia_cms

--- pychronia_cms/tests/persistent_mode_settings.py
+++ pychronia_cms/tests/persistent_mode_settings.py
@@ -10,13 +10,9 @@ ROOT_URLCONF = 'pychronia_cms.tests._test_urls'
 
 _curdir = os.path.dirname(os.path.abspath(__file__))
 DATABASES = {
-    '__default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(_curdir, "django.db")
-    },
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'chrysalis_cms_dev_empty',
+        'NAME': 'chrysaliscms_dev',
         'USER': 'root',
         'PASSWORD': 'root',
         'HOST': '127.0.0.1',
