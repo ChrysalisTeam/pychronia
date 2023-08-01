f2c6f259f5a8e3e1cb7dc92bc233e80aa5243ce2
PKL <pythoniks@gmail.com>
2023-08-01 23:09:34 +0200

    Add workaround for ABC entities removed from "collections" package

--- pychronia_game/__init__.py
+++ pychronia_game/__init__.py
@@ -1,1 +1,7 @@
 # -*- coding: utf-8 -*-
+
+
+# Workaround for ABC aliases removed in Python 3.10+
+import collections
+collections.Callable = collections.abc.Callable
+collections.Mapping = collections.abc.Mapping
